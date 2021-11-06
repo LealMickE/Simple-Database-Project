736b2319fa34f77b426260a9f4659f33956f33ca
Michael Leal <64756971+LealMickE@users.noreply.github.com>
2021-11-05 21:20:38 -0500

    Add files via upload

---
+++ frmMain.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Database_Program
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.customerNumberDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.customerLastNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.customerFirstNameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.customerLocationDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.customerTableBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.customersDataSet = new Database_Program.CustomersDataSet();
+            this.txtLocation = new System.Windows.Forms.TextBox();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.customerTableTableAdapter = new Database_Program.CustomersDataSetTableAdapters.CustomerTableTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customerTableBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customersDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.customerNumberDataGridViewTextBoxColumn,
+            this.customerLastNameDataGridViewTextBoxColumn,
+            this.customerFirstNameDataGridViewTextBoxColumn,
+            this.customerLocationDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.customerTableBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(460, 177);
+            this.dataGridView1.TabIndex = 18;
+            // 
+            // customerNumberDataGridViewTextBoxColumn
+            // 
+            this.customerNumberDataGridViewTextBoxColumn.DataPropertyName = "customerNumber";
+            this.customerNumberDataGridViewTextBoxColumn.HeaderText = "customerNumber";
+            this.customerNumberDataGridViewTextBoxColumn.Name = "customerNumberDataGridViewTextBoxColumn";
+            // 
+            // customerLastNameDataGridViewTextBoxColumn
+            // 
+            this.customerLastNameDataGridViewTextBoxColumn.DataPropertyName = "customerLastName";
+            this.customerLastNameDataGridViewTextBoxColumn.HeaderText = "customerLastName";
+            this.customerLastNameDataGridViewTextBoxColumn.Name = "customerLastNameDataGridViewTextBoxColumn";
+            // 
+            // customerFirstNameDataGridViewTextBoxColumn
+            // 
+            this.customerFirstNameDataGridViewTextBoxColumn.DataPropertyName = "customerFirstName";
+            this.customerFirstNameDataGridViewTextBoxColumn.HeaderText = "customerFirstName";
+            this.customerFirstNameDataGridViewTextBoxColumn.Name = "customerFirstNameDataGridViewTextBoxColumn";
+            // 
+            // customerLocationDataGridViewTextBoxColumn
+            // 
+            this.customerLocationDataGridViewTextBoxColumn.DataPropertyName = "customerLocation";
+            this.customerLocationDataGridViewTextBoxColumn.HeaderText = "customerLocation";
+            this.customerLocationDataGridViewTextBoxColumn.Name = "customerLocationDataGridViewTextBoxColumn";
+            // 
+            // customerTableBindingSource
+            // 
+            this.customerTableBindingSource.DataMember = "CustomerTable";
+            this.customerTableBindingSource.DataSource = this.customersDataSet;
+            // 
+            // customersDataSet
+            // 
+            this.customersDataSet.DataSetName = "CustomersDataSet";
+            this.customersDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // txtLocation
+            // 
+            this.txtLocation.Location = new System.Drawing.Point(283, 325);
+            this.txtLocation.Name = "txtLocation";
+            this.txtLocation.Size = new System.Drawing.Size(130, 20);
+            this.txtLocation.TabIndex = 17;
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(296, 257);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(117, 20);
+            this.txtNumber.TabIndex = 16;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(283, 291);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(130, 20);
+            this.txtName.TabIndex = 15;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(226, 332);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Location:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(226, 298);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Name:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(226, 264);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Customer #:";
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(12, 270);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(144, 41);
+            this.btnShow.TabIndex = 11;
+            this.btnShow.Text = "Show Record";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(12, 216);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(144, 41);
+            this.btnLoad.TabIndex = 10;
+            this.btnLoad.Text = "Load Data";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // customerTableTableAdapter
+            // 
+            this.customerTableTableAdapter.ClearBeforeFill = true;
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 391);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtLocation);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.btnLoad);
+            this.Name = "frmMain";
+            this.Text = "Database";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customerTableBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.customersDataSet)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox txtLocation;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Button btnLoad;
+        private CustomersDataSet customersDataSet;
+        private System.Windows.Forms.BindingSource customerTableBindingSource;
+        private CustomersDataSetTableAdapters.CustomerTableTableAdapter customerTableTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn customerNumberDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn customerLastNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn customerFirstNameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn customerLocationDataGridViewTextBoxColumn;
+    }
+}
+
---
+++ frmMain.cs
@@ -0,0 +1,121 @@
+﻿//Michael Leal
+//Database program
+//itse 2353
+//program will get info from a database and display it in program
+
+using System;
+using System.Data;
+using System.Data.OleDb;
+using System.Windows.Forms;
+
+namespace Database_Program
+{
+    public partial class frmMain : Form
+    {
+        private OleDbDataAdapter customerDataAdapter;
+        private DataSet customerDataSet;
+        private OleDbCommand dbCommand;
+        private OleDbConnection dbConnection;      
+
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.customerTableTableAdapter.Fill(this.customersDataSet.CustomerTable);
+            infoHide();
+            dataGridView1.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+        }
+        private void infoHide()
+        {
+            label1.Hide();
+            label2.Hide();
+            label3.Hide();
+            dataGridView1.Hide();
+            btnShow.Hide();
+            txtNumber.Hide();
+            txtName.Hide();
+            txtLocation.Hide();
+        }
+
+        private void infoShow()
+        {
+            label1.Show();
+            label2.Show();
+            label3.Show();
+            txtNumber.Show();
+            txtName.Show();
+            txtLocation.Show();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            //display dataGridView1 box
+            dataGridView1.Show();
+            //display bthShow and hide btnLoad from user
+            btnLoad.Hide();
+            btnShow.Show();
+
+            //display records from access file
+            try
+            {
+                dbConnection = new OleDbConnection("Provider = Microsoft.ACE.OLEDB.12.0;" + "Data Source = Customers.accdb");
+                dbCommand = new OleDbCommand();
+                dbCommand.CommandText = "SELECT * FROM CustomerTable ORDER BY customerLastName ASC, customerFirstName ASC;";
+
+                dbCommand.Connection = dbConnection;
+
+                //create the DataAdapter and set the select command
+                customerDataAdapter = new OleDbDataAdapter();
+                customerDataAdapter.SelectCommand = dbCommand;
+
+                //create and fill the data set
+                customerDataSet = new DataSet();
+                customerDataAdapter.Fill(customerDataSet, "CustomerTable");
+
+                dataGridView1.DataSource = customerDataSet;
+                dataGridView1.DataMember = "CustomerTable";
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            //display all infromation from selected record. 
+            infoShow();
+
+            try
+            {
+                DataGridViewRow dataInfoGrid = dataGridView1.SelectedRows[0];
+                txtNumber.Text = dataInfoGrid.Cells[0].Value.ToString();
+                txtName.Text = dataInfoGrid.Cells[1].Value.ToString() + " " + dataInfoGrid.Cells[2].Value.ToString();
+
+                switch (txtLocation.Text = dataInfoGrid.Cells[3].Value.ToString())
+                {
+                    case "N":
+                        txtLocation.Text = "North";
+                        break;
+                    case "S":
+                        txtLocation.Text = "South";
+                        break;
+                    case "E":
+                        txtLocation.Text = "East";
+                        break;
+                    case "W":
+                        txtLocation.Text = "West";
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
